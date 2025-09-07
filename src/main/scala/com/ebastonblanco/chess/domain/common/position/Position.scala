ce09192d68eebe3317804b4774a0b4d490b23491
Francisco Javier Estrada Martínez <fjestrad@outlook.com>
2025-09-07 17:58:42 +0200

    Implement FEN serialization for position

--- src/main/scala/com/ebastonblanco/chess/domain/common/position/Position.scala
+++ src/main/scala/com/ebastonblanco/chess/domain/common/position/Position.scala
@@ -4,3 +4,22 @@ import com.ebastonblanco.chess.domain.common.Color
 import com.ebastonblanco.chess.domain.common.Coordinate
 
 final case class Position(board: Board, activeSide: Color, castlingAvailability: CastlingAvailability, enPassantTarget: Option[Coordinate], halfmoveClock: Int, fullmoveNumber: Int)
+
+object Position extends FenWritter[Position]:
+
+    override def toFen(positionComponent: Position): String =
+        val boardFen = BoardFENWritter.toFen(positionComponent.board)
+
+        val activeSideFen = positionComponent.activeSide match
+        case Color.White => "w"
+        case Color.Black => "b"
+
+        val castlingAvailabilityFen = CastlingAvailability.toFen(positionComponent.castlingAvailability)
+        
+        val enPassantTargetFen = positionComponent.enPassantTarget.map(_.toString).getOrElse("-")
+        val halfmoveClockFen = positionComponent.halfmoveClock.toString
+        val fullmoveNumberFen = positionComponent.fullmoveNumber.toString
+    
+        s"$boardFen $activeSideFen $castlingAvailabilityFen $enPassantTargetFen $halfmoveClockFen $fullmoveNumberFen"
+
+        
