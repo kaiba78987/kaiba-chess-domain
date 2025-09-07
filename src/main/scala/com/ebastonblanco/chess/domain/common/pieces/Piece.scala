29012b2d648795969c52c528ae1d90149aebe726
Francisco Javier Estrada Martínez <fjestrad@outlook.com>
2025-09-07 17:11:00 +0200

    Piece definition

--- src/main/scala/com/ebastonblanco/chess/domain/common/pieces/Piece.scala
+++ src/main/scala/com/ebastonblanco/chess/domain/common/pieces/Piece.scala
@@ -1,7 +1,34 @@
 package com.ebastonblanco.chess.domain.common.pieces
 
-trait Piece
-  
+import com.ebastonblanco.chess.domain.common.Color
+import com.ebastonblanco.chess.domain.common.movement.Move
+import com.ebastonblanco.chess.domain.common.position.Position
+import com.ebastonblanco.chess.domain.common.position.FENReader
+import com.ebastonblanco.chess.domain.common.position.FenWritter
+
+final case class Piece private(private val strategy: PieceStrategy, val color: Color):
+
+  def pieceType = strategy.pieceType
+
+    def canMove(move: Move, position: Position): Boolean =
+      strategy.canMove(move, position, this)
+
+
+object Piece extends FenWritter[Piece]:
+
+    def toFen(piece: Piece): String =
+      val representation = piece.pieceType.representation
+      piece.color match
+        case Color.White => representation.toString
+        case Color.Black => representation.toLower.toString
+
+
+trait PieceStrategy:
+
+    val pieceType: PieceType
+
+    def canMove(move: Move, position: Position, self: Piece): Boolean
+
 
 enum PieceType(val representation: Char):
   
